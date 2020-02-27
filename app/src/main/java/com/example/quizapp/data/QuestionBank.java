ab9a6a9d6ebcc1933689adab1e8fac1c05930cc9
rexfilius <osakweifykay@gmail.com>
2020-02-27 11:10:37 +0100

    fixed the error in the Json request code and switched to dubsmash volley.

--- app/src/main/java/com/example/quizapp/data/QuestionBank.java
+++ app/src/main/java/com/example/quizapp/data/QuestionBank.java
@@ -20,37 +20,6 @@ public class QuestionBank {
             "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
 
     public List<Question> getQuestions(final AnswerListAsyncResponse callBack) {
-        /*JsonArrayRequest jsonArrayRequest =
-                new JsonArrayRequest(Request.Method.GET,
-                        url, null,
-                        new Response.Listener<JSONArray>() {
-                            @Override
-                            public void onResponse(JSONArray response) {
-                                for(int i = 0; i < response.length(); i++) {
-                                    try {
-                                        Question question = new Question();
-                                        question.setAnswer(
-                                                response.getJSONArray(i).get(0).toString());
-                                        question.setAnswerTrue(
-                                                response.getJSONArray(i).getBoolean(1));
-                                        questionArrayList.add(question);
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                                if(null != callBack) {
-                                    callBack.processFinished(questionArrayList);
-                                }
-
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-
-                            }
-                        });*/
-
         JsonArrayRequest jsonArrayRequest =
                 new JsonArrayRequest(Request.Method.GET, url, (JSONArray) null,
                         new Response.Listener<JSONArray>() {
